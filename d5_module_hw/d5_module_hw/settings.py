4ac07b78040c56253082c77d3530446a1b0f7aed
bulbajs <93475048+bulbajs@users.noreply.github.com>
2025-04-17 16:43:04 +0300

    d5_hw (psycopg)
    - Решена проблема с установкой пакета psycopg (Основная причина заключалась в отсутствии пути PATH C:\Program Files\PostgreSQL\17\bin для нахождения файла libpq.dll). Далее установка не psycopg2 , не psycopg2-binary , а psycopg

--- d5_module_hw/d5_module_hw/settings.py
+++ d5_module_hw/d5_module_hw/settings.py
@@ -91,6 +91,7 @@ WSGI_APPLICATION = 'd5_module_hw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -99,6 +100,14 @@ DATABASES = {
         'PASSWORD': 'elephant',  # Пароль пользователя PostgreSQL
         'HOST': 'localhost',               # Сервер базы данных (обычно localhost)
         'PORT': '5432',                    # Порт PostgreSQL (стандартный порт)
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Укажите путь к вашей SQLite-базе
     }
 }
 
